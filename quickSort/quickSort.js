6ece0b1d0b458574d2caa6dc67701d448bd69a4e
larry <larry@LarryCQ42.(none)>
2013-10-10 19:22:38 +0800

    add quick sort javascript file

---
+++ quickSort/quickSort.js
@@ -0,0 +1,45 @@
+;"use strict";
+var log=function(msg){console.log(msg);};
+var alert=function(msg){console.log(msg);};
+
+var arr = [3,6,7,5,3,6,2,9,1,5,33,-12,0,-122,-Infinity, 125, 33, 55, 77];
+
+Array.prototype.swap = function (sIdx, tIdx ) {
+	var tmp = this[sIdx];
+	this[sIdx] = this[tIdx];
+	this[tIdx] = tmp; 
+}
+
+Array.prototype.quickSort = function () {
+	var arr = this;
+
+	_quickSort(0, arr.length -1);
+
+	function _quickSort(leftIdx, rightIdx) {
+		if(leftIdx < rightIdx) {
+			var pivot = arr[leftIdx],
+			chkLeftIdx = leftIdx,
+			chkRightIdx = rightIdx;
+
+			while(chkLeftIdx < chkRightIdx) {
+				while(arr[chkRightIdx] > pivot) {
+					chkRightIdx--;
+				}
+				while(chkLeftIdx < chkRightIdx && arr[chkLeftIdx] <= pivot) {
+					chkLeftIdx++;
+				}
+				
+				arr.swap(chkLeftIdx, chkRightIdx);
+			}
+
+			arr.swap(leftIdx, chkLeftIdx);
+
+			_quickSort(leftIdx, chkLeftIdx-1);
+			_quickSort(chkLeftIdx+1, rightIdx);
+		}
+	}
+
+	return arr;
+};
+alert(arr.sort());
+alert(arr.quickSort());
