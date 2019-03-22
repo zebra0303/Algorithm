1ec9942a1ec6834537e1cb4daa4e3e1620b6bab5
Larry Kim <larry@evasion.co.kr>
2019-03-22 10:38:08 +0900

    optimize loop

--- ProjectEuler/43.Sub-string divisibility.js
+++ ProjectEuler/43.Sub-string divisibility.js
@@ -1,5 +1,6 @@
 var log = console.log,
   arr_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  arr_pnum = [2, 3, 5, 7, 11, 13, 17],
   ans = 0;
 
 function make_pd_case(arr_new, arr_old) {
@@ -13,19 +14,7 @@ function make_pd_case(arr_new, arr_old) {
   } else {
     for (i = 0; i < len_arr_old; i++) {
       num = arr_old[i];
-      if (len_arr_new === 3 && parseInt(arr_new[1] + "" + arr_new[2] + "" + num, 10) % 2 !== 0) {
-        continue;
-      } else if (len_arr_new === 4 && parseInt(arr_new[2] + "" + arr_new[3] + "" + num, 10) % 3 !== 0) {
-        continue;
-      } else if (len_arr_new === 5 && parseInt(arr_new[3] + "" + arr_new[4] + "" + num, 10) % 5 !== 0) {
-        continue;
-      } else if (len_arr_new === 6 && parseInt(arr_new[4] + "" + arr_new[5] + "" + num, 10) % 7 !== 0) {
-        continue;
-      } else if (len_arr_new === 7 && parseInt(arr_new[5] + "" + arr_new[6] + "" + num, 10) % 11 !== 0) {
-        continue;
-      } else if (len_arr_new === 8 && parseInt(arr_new[6] + "" + arr_new[7] + "" + num, 10) % 13 !== 0) {
-        continue;
-      } else if (len_arr_new === 9 && parseInt(arr_new[7] + "" + arr_new[8] + "" + num, 10) % 17 !== 0) {
+      if(len_arr_new >= 3 && parseInt(arr_new[len_arr_new - 2] + "" + arr_new[len_arr_new - 1] + "" + num, 10) % arr_pnum[len_arr_new - 3] !== 0) {
         continue;
       }
 
