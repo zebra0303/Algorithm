bcb4331985bcff6eee79d00917be8b2d005d623a
seyoun.kim <zebra0303@gmail.com>
2020-03-20 13:27:17 +0900

    조금 더 개선하라

--- ProjectEuler/52.Permuted_multiples.js
+++ ProjectEuler/52.Permuted_multiples.js
@@ -2,25 +2,27 @@ const log = console.log;
 
 function getSorted(num) {
   const strNum = num + '';
+
   return strNum.split('').sort().join('');
 }
 
 function chkNum(num) {
-  const originNum = num;
   let cntPass = 0;
+  const strNum = getSorted(num);
   const arrMultiplyNum = [2, 3, 4, 5, 6];
-  const strOriginNum = getSorted(originNum);
-  for(const mNum of arrMultiplyNum) {
-    if(strOriginNum === getSorted(originNum*mNum)) {
+  for (const mNum of arrMultiplyNum) {
+    if (strNum === getSorted(num * mNum)) {
       cntPass++;
+    } else {
+      break;
     }
   }
 
-  return (cntPass === arrMultiplyNum.length) ? true : false;
+  return cntPass === arrMultiplyNum.length;
 }
 
 let num = 1;
-while(!chkNum(num)) {
+while (!chkNum(num)) {
   num++;
 }
-log(num);
+log(`The answer is ${num}`);
