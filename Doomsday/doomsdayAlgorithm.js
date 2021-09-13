6d85f6f118c62088220c0ae76e37ec9610afeba5
seyoun.kim <zebra0303@gmail.com>
2021-09-13 22:48:09 +0900

    add doomsday algorithm files

---
+++ Doomsday/doomsdayAlgorithm.js
@@ -0,0 +1,20 @@
+const log = console.log;
+const arrWeekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const arrAnchorday = [2, 0, 5, 3]; // 엥커데이 년도 앞2자리를 4로 나누면 나머지의 요일값 '화(2), 일(0), 금(5), 수(3)'가 계속 반 복됨
+
+const strYear = (new Date()).getFullYear() + ''; // 올해 년도 값
+// 년도 뒤 2자리 숫자와 그 외 앞자리 숫자를 구분
+const [, strYearFront, strYearEnd] = strYear.split(/^(\d+)(\d{2})$/);
+// 1. 년도 뒷자리 2 숫자를 12로 나눈 몫 : 12년 마다 하루씩 증가
+const numQuotient = Math.floor(parseInt(strYearEnd, 10)/12);
+// 2. 년도 뒷자리 2 숫자를 12로 나눈 나머지 : 매년 하루씩 증가
+const numRemainder = parseInt(strYearEnd, 10)%12;
+// 3. 위 나머지를 4로 나눈 값 : 4년마다 유년이 있음
+const numQuotient4Remainder = Math.floor(numRemainder/4);
+// 4. 년도 뒤 2자리를 제외한 앞자리의 엥커데이 (400년 마다 순환됨)
+const numAnchorday = arrAnchorday[parseInt(strYearFront, 10)%4];
+// 5. 위에서 구한 모든 값을 더한뒤 7로 나눈 나머지가 둠스 데이!!!
+const doomsday = arrWeekday[(numQuotient + numRemainder + numQuotient4Remainder + numAnchorday)%7];
+
+//log(strYearFront, strYearEnd, numQuotient, numRemainder, numQuotient4Remainder, numAnchorday, doomsday);
+log(`${strYear}'s doomsday is ${doomsday}!!!`);
---
+++ Doomsday/doomsdayAlgorithm2.js
@@ -0,0 +1,37 @@
+// https://arxiv.org/ftp/arxiv/papers/1010/1010.0765.pdf 암산으로 계산하기 쉬운 공식
+
+const log = console.log;
+const arrWeekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const arrAnchorday = [2, 0, 5, 3]; // 엥커데이 년도 앞2자리를 4로 나누면 나머지의 요일값 '화(2), 일(0), 금(5), 수(3)'가 계속 반 복됨
+
+const strYear = (new Date()).getFullYear() + ''; // 올해 년도 값
+// 년도 뒤 2자리 숫자와 그 외 앞자리 숫자를 구분
+const [, strYearFront, strYearEnd] = strYear.split(/^(\d+)(\d{2})$/);
+
+let numWeek = parseInt(strYearEnd, 10);
+
+// 1. 년도수 끝 2자리가 홀수라면 11을 더한다
+if (numWeek%2 !== 0) {
+  numWeek += 11;
+}
+
+// 2. 위에서 구한 값을 2로 나눈다
+numWeek /= 2;
+
+// 3. 위에서 나눈 값이 홀수면 11을 더한다
+if (numWeek%2 !== 0) {
+  numWeek += 11;
+}
+
+// 4. 위에서 구한 값을 7로 나눈 나머지의 7의 보수를 구한다
+numWeek = 7 - numWeek%7;
+
+// 5. 년도 뒤 2자리를 제외한 앞자리의 엥커데이 (400년 마다 순환됨)
+const numAnchorday = arrAnchorday[parseInt(strYearFront, 10)%4];
+
+// 6. 엥커데이와 7의보수를 더한값의 7로 나눈 나머지가 둠스데이!!!
+const doomsday = arrWeekday[(numAnchorday + numWeek)%7];
+
+//log(numWeek, numAnchorday, doomsday);
+
+log(`${strYear}'s doomsday is ${doomsday}!!!`);
