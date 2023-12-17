bfeceab136bb24d1fa774cc625652bbda3a3a4fb
Larry Kim <zebra0303@gmail.com>
2023-12-17 12:24:47 +0900

    add files

---
+++ Recursive/permutation.js
@@ -0,0 +1,45 @@
+const log = console.log;
+
+// https://www.youtube.com/watch?v=0tcgYHU8IIs
+const permutations01 = (arr) => {
+  const result = [];
+
+  const permute = (i, arr) => {
+    if (i === arr.length) {
+      result.push([...arr]);
+    }
+
+    for (let j = i; j < arr.length; j++) {
+      // swap
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      permute(i + 1, arr);
+      // swap back;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  };
+
+  permute(0, arr);
+
+  return result;
+};
+
+// https://www.youtube.com/watch?v=us0cYQXQpxg
+const permutations02 = (arr) => {
+  if (arr.length === 0) return [[]];
+
+  let [first, ...rest] = arr;
+
+  const permsRest = permutations02(rest);
+  const result = [];
+
+  permsRest.forEach((perm) => {
+    for (let i = 0; i <= perm.length; i++) {
+      result.push([...perm.slice(0, i), first, ...perm.slice(i)]);
+    }
+  });
+
+  return result;
+};
+
+log(permutations01(['a', 'b', 'c']));
+log(permutations02(['a', 'b', 'c']));
